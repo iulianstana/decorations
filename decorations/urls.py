231e9d50f5a5f6137daeca9eac9f7d68e8f5bf7a
Iulian <julian.stana@gmail.com>
2013-12-30 22:41:53 +0200

    index page with no content

--- decorations/urls.py
+++ decorations/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from frontend.views import IndexView
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'decorations.views.home', name='home'),
+    url(r'^$', IndexView.as_view(), name='index'),
     # url(r'^blog/', include('blog.urls')),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request):
+        params = {}
+        return render(request, self.template_name, params)
+
+    def post(self, request):
+        return redirect("index")
+
 
 # Create your views here.
