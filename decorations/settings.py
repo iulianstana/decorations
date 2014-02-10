4a662ca99890781909ce195c79e75a9b50c57285
Iulian <julian.stana@gmail.com>
2014-02-10 16:37:41 +0200

    create a base site template
    It's the first model of my site template. Also I added two .jpg images one for
    background image and one for header image.
    
    The header image it's a random one, it will require a change.

--- decorations/settings.py
+++ decorations/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'server',
     'frontend',
 )
@@ -86,7 +87,7 @@ STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -96,7 +97,6 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'frontend', 'static'),
