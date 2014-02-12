97809604562efd2a984efe64cc3b2ff653ddf0d9
Iulian <julian.stana@gmail.com>
2014-02-12 15:22:09 +0200

    adding some categories in category-bar

--- frontend/views.py
+++ frontend/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from server.models import Category
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
     def get(self, request):
-        params = {}
+        category = Category.objects.all().order_by('pozition')
+        params = {
+            'category': category,
+            'background': 'img/background.jpg',
+        }
         return render(request, self.template_name, params)
 
     def post(self, request):
+
         return redirect("index")
 
 
