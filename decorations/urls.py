dfb4cf04f8b3ea1e1e1619391428390c92fecdfc
Iulian <julian.stana@gmail.com>
2014-02-15 01:02:06 +0200

    add an url for categories view

--- decorations/urls.py
+++ decorations/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 
-from frontend.views import IndexView
+from frontend.views import IndexView, CategoryView
 
 admin.autodiscover()
 
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', IndexView.as_view(), name='index'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^category/(?P<cat_id>\d+)', CategoryView.as_view(), name='category'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
