5fa85426abe916245a7cdf3ef26ca4898c33f8c3
Iulian <julian.stana@gmail.com>
2014-02-15 01:01:22 +0200

    create a template view for categories

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from server.models import Category
+from server.models import Category, Image
 
 
 class IndexView(TemplateView):
@@ -10,7 +10,6 @@ class IndexView(TemplateView):
         category = Category.objects.all().order_by('pozition')
         params = {
             'category': category,
-            'background': 'img/background.jpg',
         }
         return render(request, self.template_name, params)
 
@@ -18,5 +17,22 @@ class IndexView(TemplateView):
 
         return redirect("index")
 
+
+class CategoryView(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, cat_id):
+        category = Category.objects.all()
+        images = Image.objects.filter(category_id = cat_id)
+        print images
+        params = {
+            'category': category,
+            'images': images,
+        }
+
+        return render(request, self.template_name, params)
+
+    def post(self, request):
+        return redirect("category")
 
 # Create your views here.
